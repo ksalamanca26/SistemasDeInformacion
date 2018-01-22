22b76edd8d4d4dea6c01765647f45deee6d9ac1c
Kevin Salamanca <k.salamanca@correo.unimet.edu.ve>
2018-01-22 15:50:01 -0400

    Interfaz prototipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { MaterializeModule } from "angular2-materialize";
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { InicioComponent } from './inicio/inicio.component';
 
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  { path: 'registro-usuario', component: RegistroUsuarioComponent },
+  { path: 'inicio', component: InicioComponent },
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    RegistroUsuarioComponent,
+    InicioComponent
   ],
-  imports: [
+  imports: [RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     MaterializeModule
   ],
